0f679510c9b7e31d71685ae28cddbea89c64c613
Mohammad Azmi <mazmiagus2003@gmail.com>
2025-11-14 03:18:19 +0700

    migrate os to arch, and a working nvim (finally)

---
+++ .config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,65 @@
+return {
+  "hrsh7th/nvim-cmp",
+  -- load cmp on InsertEnter
+  -- event = "InsertEnter",
+  -- these dependencies will only be loaded when cmp loads
+  -- dependencies are always lazy-loaded unless specified otherwise
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+     "hrsh7th/cmp-calc",
+    "jcha0713/cmp-tw2css",
+  },
+  config = function()
+    local cmp = require 'cmp'
+    cmp.setup({
+      snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+          vim.snippet.expand(args.body)
+          -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
+      },
+      window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'calc' },
+        { name = 'cmp-tw2css' },
+        -- { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'codeium' },
+      }, {
+        { name = 'buffer' }
+      })
+    })
+
+    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline({ '/', '?' }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' }
+      }
+    })
+
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        { name = 'cmdline' },
+      })
+    })
+  end,
+}
---
+++ .config/nvim/lua/plugins/finder.lua
@@ -0,0 +1,64 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = 'v0.1.9',
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install'
+    },
+  },
+  config = function()
+    require("telescope").setup({
+      path_display = { "smart" },
+      file_ignore_patterns = { "node_modules" },
+      layout_config = {
+        horizontal = {
+          preview_cutoff = 0,
+        },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,                    -- false will only do exact matching
+          override_generic_sorter = true,  -- override the generic sorter
+          override_file_sorter = true,     -- override the file sorter
+          case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+          -- the default case_mode is "smart_case"
+        },
+      },
+    })
+
+    require("telescope").load_extension("fzf")
+
+    local telescope = require('telescope.builtin')
+    local used = ""
+    local function telescope_find_files()
+      if used ~= "find_files" then
+        used = "find_files"
+        telescope.find_files()
+      else
+        telescope.resume()
+      end
+    end
+    local function telescope_live_grep()
+      if used ~= "live_grep" then
+        used = "live_grep"
+        telescope.live_grep()
+      else
+        telescope.resume()
+      end
+    end
+    local function telescope_buffers()
+      if used ~= "buffers" then
+        used = "buffers"
+        telescope.buffers()
+      else
+        telescope.resume()
+      end
+    end
+
+    vim.keymap.set("n", "<C-P>", telescope_find_files)
+    vim.keymap.set("n", "<leader>fg", telescope_live_grep)
+    vim.keymap.set("n", "<leader>fb", telescope_buffers)
+  end,
+}
